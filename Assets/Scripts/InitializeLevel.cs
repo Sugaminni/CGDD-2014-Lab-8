ef5727e36bffc30570f1f4866a75569c5ad2de15
Alex Lu <Alexhonglu@gmail.com>
2025-11-16 00:41:15 -0500

    Created initial commit

---
+++ Assets/Scripts/InitializeLevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitializeLevel : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		// Create the walls and pillars
+		for (int i = 0; i < 1000; i+=10) {
+			GameObject leftPillar = (GameObject)Instantiate(Resources.Load("Pillar"));
+			leftPillar.transform.position = new Vector3(-5.0f, 0, i);
+			GameObject rightPillar = (GameObject)Instantiate(Resources.Load("Pillar"));
+			rightPillar.transform.position = new Vector3(5.0f, 0, i);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RotateYScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateYScript : MonoBehaviour {
+
+	public Vector3 rotationVector;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rotationVector.x, rotationVector.y, rotationVector.z);
+	}
+}
